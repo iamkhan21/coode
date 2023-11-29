2185cbbeb38b2e52292842a400fb877de3f0dfb1
Ivan Khanevich <mail@iamkhan.tech>
2023-11-29 11:32:18 +0400

    feat: add scripts for fetching translations

--- src/commands/poly/command.js
+++ src/commands/poly/command.js
@@ -1,7 +1,81 @@
+import { randomUUID } from "node:crypto";
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { command } from "cleye";
+import { run as jscodeshift } from "jscodeshift/src/Runner.js";
 
-function poly(argv) {
-	console.log(argv);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function fetchTextsFromJSXElements({
+	scanDir = "src",
+	moduleName = "translations",
+	importName = "translations",
+	tempTranslationsDir,
+} = {}) {
+	const TRANSFORM_PATH = path.join(__dirname, "./task.cjs");
+	const FILE_PATH = [path.resolve(process.cwd(), scanDir)];
+
+	const options = {
+		extensions: "tsx",
+		parser: "tsx",
+		meta: {
+			moduleName,
+			importName,
+			tempTranslationsDir,
+		},
+	};
+
+	return jscodeshift(TRANSFORM_PATH, FILE_PATH, options);
+}
+
+function uniteTranslationsIntoOneFile({
+	translationFilePath = "translations/en.json",
+	tempTranslationsDir,
+} = {}) {
+	const finalTranslationFilePath = path.resolve(
+		process.cwd(),
+		translationFilePath,
+	);
+
+	// Read existing final translations
+	let finalTranslations = {};
+	if (fs.existsSync(finalTranslationFilePath)) {
+		finalTranslations = JSON.parse(
+			fs.readFileSync(finalTranslationFilePath, "utf8"),
+		);
+	}
+
+	const files = fs.readdirSync(tempTranslationsDir);
+
+	for (const file of files) {
+		const filePath = path.join(tempTranslationsDir, file);
+		const fileTranslations = JSON.parse(fs.readFileSync(filePath, "utf8"));
+
+		for (const pageNameAbbr of Object.keys(fileTranslations)) {
+			finalTranslations[pageNameAbbr] = {
+				...finalTranslations[pageNameAbbr],
+				...fileTranslations[pageNameAbbr],
+			};
+		}
+	}
+
+	// Delete the temp dir after all merges
+	fs.rmSync(tempTranslationsDir, { recursive: true });
+
+	// Write the merged translations to the final file
+	fs.writeFileSync(
+		finalTranslationFilePath,
+		JSON.stringify(finalTranslations, null, 2),
+	);
+}
+
+async function poly(argv) {
+	const tempTranslationsDir = path.resolve(process.cwd(), randomUUID());
+
+	await fetchTextsFromJSXElements({ tempTranslationsDir });
+	uniteTranslationsIntoOneFile({ tempTranslationsDir });
 }
 
 const polyCommand = command(
---
+++ src/commands/poly/task.cjs
@@ -0,0 +1,149 @@
+const fs = require("node:fs");
+const path = require("node:path");
+
+/**
+ * Converts a given string to snake case.
+ *
+ * @param {string} str - The string to be converted.
+ * @returns {string} - The converted string in snake case.
+ */
+function convertToSnakeCase(str) {
+	return (
+		str
+			// Insert an underscore before each uppercase letter and convert the letter to lowercase
+			.replace(/([A-Z])/g, "_$1")
+			// Replace spaces, hyphens, and other non-alphanumeric characters (excluding underscores) with underscores
+			.replace(/[\s\W-]+/g, "_")
+			// Convert to lowercase
+			.toLowerCase()
+			// Remove leading or trailing underscores
+			.replace(/^_+|_+$/g, "")
+			// Replace multiple consecutive underscores with a single one
+			.replace(/_+/g, "_")
+	);
+}
+
+/**
+ * Returns the abbreviated name of a page based on the given file path.
+ *
+ * @param {string} filePath - The path of the file.
+ * @returns {string} The abbreviated page name.
+ */
+function getPageNameAbbr(filePath) {
+	const relativePath = path.relative(process.cwd(), filePath);
+	const dirs = relativePath.split(path.sep);
+	const relevantDirs = dirs.slice(1, -1);
+	const fileName = path.basename(filePath, path.extname(filePath));
+	return [...relevantDirs, fileName].map(convertToSnakeCase).join("__");
+}
+
+/**
+ * Returns the abbreviated form of a given text.
+ *
+ * @param {string} text - The text to be abbreviated.
+ * @returns {string} - The abbreviated form of the given text.
+ */
+function getTextAbbr(text) {
+	return convertToSnakeCase(text.split(".")[0].trim().toLowerCase());
+}
+
+module.exports = (fileInfo, api, options) => {
+	// Define the module and the import name
+	const { moduleName, tempTranslationsDir, importName } = options.meta;
+
+	const j = api.jscodeshift;
+	const root = j(fileInfo.source);
+
+	if (!fs.existsSync(tempTranslationsDir)) {
+		fs.mkdirSync(tempTranslationsDir, { recursive: true });
+	}
+
+	const pageNameAbbr = getPageNameAbbr(fileInfo.path);
+
+	const translations = {};
+
+	// biome-ignore lint: forEach is inbuilt method to iterate nodes
+	root
+		.find(j.JSXElement)
+		.find(j.JSXText)
+		.forEach((path) => {
+			const text = path.node.value?.trim();
+
+			if (text) {
+				const textAbbr = getTextAbbr(text);
+
+				if (!translations[textAbbr]) {
+					translations[textAbbr] = text;
+
+					// Replace text in original file
+					j(path).replaceWith(
+						j.jsxExpressionContainer(
+							j.memberExpression(
+								j.memberExpression(
+									j.identifier(importName),
+									j.stringLiteral(pageNameAbbr),
+									true,
+								),
+								j.stringLiteral(textAbbr),
+								true,
+							),
+						),
+					);
+				}
+			}
+		});
+
+	if (!Object.keys(translations).length) {
+		return root.source;
+	}
+
+	// Check if the import already exists
+	const alreadyImported =
+		root
+			.find(j.ImportDeclaration, {
+				source: {
+					type: "Literal",
+					value: moduleName,
+				},
+			})
+			.filter((path) => {
+				return path.node.specifiers.some(
+					(specifier) =>
+						specifier.type === "ImportDefaultSpecifier" &&
+						specifier.local.name === importName,
+				);
+			})
+			.size() > 0;
+
+	if (!alreadyImported) {
+		// Define the new import statement
+		const newImport = j.importDeclaration(
+			[j.importDefaultSpecifier(j.identifier(importName))],
+			j.literal(moduleName),
+		);
+
+		// Find the first import declaration in the file
+		const firstImport = root.find(j.ImportDeclaration).at(0);
+
+		if (firstImport.size() !== 0) {
+			// If there's at least one import, insert the new import before the first one
+			firstImport.insertBefore(newImport);
+		} else {
+			// If there are no import statements, insert the new import at the top of the file
+			root.get().node.program.body.unshift(newImport);
+		}
+	}
+
+	// Write translations for this file to a temporary file
+	const tempTranslationFilePath = path.join(
+		tempTranslationsDir,
+		`${path.basename(fileInfo.path)}.json`,
+	);
+
+	fs.writeFileSync(
+		tempTranslationFilePath,
+		JSON.stringify({ [getPageNameAbbr(fileInfo.path)]: translations }, null, 2),
+	);
+
+	return root.toSource();
+};
